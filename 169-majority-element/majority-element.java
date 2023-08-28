09ff687d484bf4bb8c6a9c1e5c3c560b1421db92
Eshu Dhiman <63957845+EshuDhiman@users.noreply.github.com>
2023-08-28 21:01:28 +0530

    Time: 6 ms (38.71%) | Memory: 48.6 MB (34.42%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -5,8 +5,10 @@ class Solution {
         Arrays.sort(nums); 
         for(int i = 1; i<nums.length;i++){
             if(nums[i-1]==nums[i]){
+                // here we used jab tk k ki vlaue array ke half size se choti h tb tk usko increment kro vrna jab k ki value increase ho gai half of the array size se to increment na kro is se hoga kya agr kuch cases me elements ki jo freq hai wo jda hai to kai baar cases me sahi postion na return krte hue uske ek aage ki value return kr rha tha to mene ye conditon lga di
                 if(k<nums.length/2){k++;}
             }
+            // 
             if(k>((nums.length)/(float)(2))){
                 break;
             }
