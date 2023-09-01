e67e7bfa766be74ea9519add117e58aed4dd8fd2
Eshu Dhiman <63957845+EshuDhiman@users.noreply.github.com>
2023-09-01 20:25:04 +0530

    Time: 4 ms (76.75%) | Memory: 44.1 MB (11.62%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,44 +1,29 @@
 class Solution {
     public int romanToInt(String s) {
-       HashMap<String, Integer> map = new HashMap<>();
-        map.put("I", 1);
-        map.put("V", 5);
-        map.put("X", 10);
-        map.put("L", 50);
-        map.put("C", 100);
-        map.put("D", 500);
-        map.put("M", 1000);     
-        // char[] key = s.toCharArray();
-        
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
         int sum = 0;
-        for (int i = 0,j=1; i < s.length(); i++) {
-            String a = s.substring(i, j);
-            System.out.print(a);
-            if(map.containsKey(a)){
-                sum=sum+map.get(a);
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currValue = map.get(s.charAt(i));
+
+            if (currValue < prevValue) {
+                sum -= currValue;
+            } else {
+                sum += currValue;
             }
-            j++; 
+
+            prevValue = currValue;
         }
-        if (s.contains("IV")) {
-            sum=sum-2;
-        }
-        if (s.contains("IX")) {
-            sum=sum-2;
-        }
-        if (s.contains("XL")) {
-            sum=sum-20;
-        }
-        if (s.contains("XC")) {
-            sum=sum-20;
-        }
-        if (s.contains("CD")) {
-            sum=sum-200;
-        }
-        if (s.contains("CM")) {
-            sum=sum-200;
-        }
-        // System.out.println(sum);
+
         return sum;
-        
     }
 }
